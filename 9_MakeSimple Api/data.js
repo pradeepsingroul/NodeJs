9626cd5a041290df6fa250ac117446b72ba37225
predeep kumar singroul <prasingroul4899@gmail.com>
2023-10-03 17:37:32 +0530

    Making a simple api

---
+++ 9_MakeSimple Api/data.js
@@ -0,0 +1,31 @@
+const arr = []
+const objectData1 = {
+    name: "Pradeep Singroul",
+    age: 24,
+    address: "Satna mp India",
+    email: "pradeep123@gmail.com"
+}
+const objectData2 = {
+    name: "Preep Singroul",
+    age: 24,
+    address: "Satna mp India",
+    email: "preep123@gmail.com"
+}
+const objectData3 = {
+    name: "reep Singroul",
+    age: 22,
+    address: "Satna mp India",
+    email: "reep123@gmail.com"
+}
+const objectData4 = {
+    name: "deep Singroul",
+    age: 20,
+    address: "Satna mp India",
+    email: "deep123@gmail.com"
+}
+arr.push(objectData1)
+arr.push(objectData2)
+arr.push(objectData3)
+arr.push(objectData4)
+
+module.exports ={ arr };
