5ddecce07351b1ed6045a8f3a3fa1113db6483d5
predeep kumar singroul <prasingroul4899@gmail.com>
2023-10-04 11:32:58 +0530

    removing extension from the url and try to sendFile using app.get() method

--- 17_Making html Pages/index.js
+++ 17_Making html Pages/index.js
@@ -3,6 +3,26 @@ var app = express();
 const path = require("path")
 var path1 = path.join(__dirname,"public");
 
-app.use(express.static(path1))
+// app.use(express.static(path1))
+/*it is used to load the static data of file which are present insidde the path
+localhost:8080/index.html
+localhost:8080/login.html
+localhost:8080/signup.html*/
+
+// if we want to remove the extension like localhost:80800/index then
+//we can send the file using get methos as well
+app.get("",(req,res)=>{//localhost:8080 it return index.html page
+    res.sendFile(`${path1}/index.html`)
+})
+app.get("/signup",(req,res)=>{//localhost:8080/signup it return signup.html page
+    res.sendFile(`${path1}/signup.html`)
+})
+app.get("/login",(req,res)=>{//localhost:8080/login it return login.html page
+    res.sendFile(`${path1}/login.html`)
+})
+
+app.get("*",(req,res)=>{//if we write wrong url then 404.html will return
+    res.sendFile(`${path1}/404.html`)
+})
 app.listen(8080)
 
